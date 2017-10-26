4e750abb894656ec419478da1f86c9b7cdf2dba1
Cameron Doggett <camd.99@gmail.com>
2017-10-26 01:49:39 -0500

    Syntax fix

--- util/addUserAdmin.js
+++ util/addUserAdmin.js
@@ -9,7 +9,7 @@ mongodb.connect(config.database.url, (err, db)=>{
             username: "admin",
             password: "admin",
             admin: true
-        }
+        };
         collection.insertOne(admin, (err, result)=>{
             if(err) throw err;
             console.log("Inserted " + result.insertedCount + " admin");
